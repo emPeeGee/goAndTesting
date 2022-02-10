45447453a1996ca30de81f1cbe540b476a3ed0f2
emPeeGee <eempeegee@gmail.com>
2022-02-10 09:18:32 +0200

    feat: Add mocking and tests

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module go_and_test
 
 go 1.17
+
+require (
+	github.com/kyoh86/richgo v0.3.10 // indirect
+	github.com/kyoh86/xdg v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/wacul/ptr v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_and_test/mocking"
+	"os"
+	"time"
+)
 
 const spanish = "Spanish"
 const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() { time.Sleep(1 * time.Second) }
 
 func Hello(name, language string) string {
 	if name == "" {
@@ -31,4 +40,7 @@ func greetingPrefix(language string) (prefix string) {
 
 func main() {
 	fmt.Println(Hello("There", "English"))
+
+	sleeper := &DefaultSleeper{}
+	mocking.Countdown(os.Stdout, sleeper)
 }
